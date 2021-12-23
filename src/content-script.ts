6eba6bc87c922a0886eec5cda88bfdac8d1dccff
Andreas Dzialocha <x12@adz.garden>
2021-12-23 22:22:01 +0100

    Small experiments

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,3 +1,20 @@
-window.addEventListener('load', () => {
-  document.body.style.border = '5px solid red';
+const editor = document.createElement('div');
+editor.style.border = '5px solid red';
+editor.style.position = 'absolute';
+editor.style.zIndex = '999999';
+editor.style.pointerEvents = 'none';
+editor.style.userSelect = 'none';
+document.body.appendChild(editor);
+
+window.addEventListener('mouseover', (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  const target = event.target as HTMLElement;
+  const { x, y, width, height } = target.getBoundingClientRect();
+
+  editor.style.top = `${window.scrollY + y}px`;
+  editor.style.left = `${window.scrollX + x}px`;
+  editor.style.width = `${width}px`;
+  editor.style.height = `${height}px`;
 });
