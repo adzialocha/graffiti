9b9666c5f727c0292d94a54ea5d6c80934ac4290
Andreas Dzialocha <x12@adz.garden>
2022-03-11 00:50:32 +0100

    Add toolbar component

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
 import { css, keyframes } from '@emotion/react';
 import { finder as getCssSelector } from '@medv/finder';
 
 import { Context } from '~/components/ContextProvider';
+
+// If a clicked target is too close to the top or right side of the page, the
+// toolbar will move to another position to stay visible to the user.
+const TOOLBAR_THRESHOLD = 100; // px
 
 const colorKeyframes = keyframes`
   0% {
@@ -32,9 +36,57 @@ const selectorStyles = css`
   animation: ${colorKeyframes} 5s linear infinite alternate;
 `;
 
+const selectorToolbarStyles = css`
+  position: absolute;
+  pointer-events: all;
+  width: 150px;
+`;
+
+type ToolbarProps = {
+  // Show toolbar underneath target
+  bottom: boolean;
+
+  // Show toolbar inside of target
+  inside: boolean;
+
+  // Show toolbar left of target
+  left: boolean;
+
+  // Reset selected target
+  onReset: () => void;
+};
+
+const SelectorToolbar = React.forwardRef<HTMLDivElement, ToolbarProps>(
+  ({ onReset, left, bottom, inside }, ref) => {
+    // Move toolbar when element is too close to the border of the page
+    const positionX = left ? 'right: 100%;' : null;
+    let positionY = 'top: -30px;';
+    if (inside) {
+      positionY = 'top: 0;';
+    } else if (bottom) {
+      positionY = 'bottom: -30px;';
+    }
+
+    return (
+      <div
+        css={css`
+          ${selectorToolbarStyles}
+          ${positionY}
+          ${positionX}
+        `}
+        ref={ref}
+      >
+        <button>Paint</button>
+        <button>Music</button>
+        <button onClick={onReset}>Return</button>
+      </div>
+    );
+  },
+);
+
 const Selector: React.FunctionComponent = () => {
   const { target, setState } = useContext(Context);
-
+  const toolbarRef = useRef<HTMLDivElement>(null);
   const [coordinates, setCoordinates] = useState({
     x: 0,
     y: 0,
@@ -42,11 +94,21 @@ const Selector: React.FunctionComponent = () => {
     height: 0,
   });
 
+  const onReset = () => {
+    setState({
+      target: undefined,
+    });
+
+    setCoordinates({
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+    });
+  };
+
   useEffect(() => {
     const onMouseMove = (event: Event) => {
-      event.preventDefault();
-      event.stopPropagation();
-
       if (target) {
         // Do not select anything new if we already have something
         return;
@@ -83,24 +145,32 @@ const Selector: React.FunctionComponent = () => {
     };
 
     const onClick = (event: Event) => {
-      event.preventDefault();
-      event.stopPropagation();
-
       if (target) {
         // Do not select anything new if we already have something
         return;
+      } else {
+        // Ignore every click event, except of when we show the UI
+        event.preventDefault();
+        event.stopPropagation();
       }
 
+      const newTarget = event.target as HTMLElement;
+
       setState({
-        target: event.target as HTMLElement,
+        target: newTarget,
       });
 
-      const selector = getCssSelector(event.target as HTMLElement, {
+      const selector = getCssSelector(newTarget, {
+        // @TODO: Find good values
+        //
         // Use high values to disable optimizations, we actually want the full
         // and not shortened one to keep the knowledge about "where" the
         // element sits.
-        seedMinLength: 100,
-        optimizedMinLength: 100,
+        //
+        // seedMinLength: 100,
+        // optimizedMinLength: 100,
+        // threshold: 250,
+        // maxNumberOfTries: 1000,
       });
 
       console.log(selector);
@@ -128,7 +198,17 @@ const Selector: React.FunctionComponent = () => {
         width: ${coordinates.width + selectorBorder * 2}px;
         height: ${coordinates.height + selectorBorder * 2}px;
       `}
-    />
+    >
+      {target ? (
+        <SelectorToolbar
+          ref={toolbarRef}
+          onReset={onReset}
+          bottom={coordinates.y < TOOLBAR_THRESHOLD}
+          inside={coordinates.height > TOOLBAR_THRESHOLD}
+          left={coordinates.x > window.innerWidth - TOOLBAR_THRESHOLD}
+        />
+      ) : null}
+    </div>
   );
 };
 
